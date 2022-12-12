c14ea828926970970bddd5ccf5c495563ce082e2
Punkimen <66203124+Punkimen@users.noreply.github.com>
2022-12-12 18:14:07 +0300

    change styles for take-photo.html

--- #src/js/photo.js
+++ #src/js/photo.js
@@ -1,38 +1,38 @@
 const snapPhoto = (webcam, canvas) => {
-  webcam.snap();
-  $(canvas).addClass('show')
-  $('.take-photo__instruction').hide()
-  $('.take-photo__repeat').addClass('show')
-}
+	webcam.snap();
+	$(canvas).addClass("show");
+	$(".take-photo__instruction").hide();
+	$(".take-photo__repeat").addClass("show");
+};
 
 const webcamStartFirst = () => {
-  const webcamElement = document.querySelector('#webcam');
-  const canvasElement = document.querySelector('#canvas');
-  const snap = document.querySelector('#photo-do')
-  const webcam = new Webcam(webcamElement, 'user', canvasElement);
-  webcam.start()
-    .then(result => {
-      console.log("webcam started");
-      $('.take-photo__error').hide()
-    })
-    .catch(err => {
-      $('.take-photo__error').show()
-      snap.textContent = "Як надати доступ до камери"
-    });
-  snap.addEventListener('click', function () {
-    snapPhoto(webcam, '#canvas')
-    snap.textContent = "Продовжити"
-  })
-
+	const webcamElement = document.querySelector("#webcam");
+	const canvasElement = document.querySelector("#canvas");
+	const snap = document.querySelector("#photo-do");
+	const webcam = new Webcam(webcamElement, "user", canvasElement);
+	webcam
+		.start()
+		.then((result) => {
+			console.log("webcam started");
+			$(".take-photo__error").hide();
+		})
+		.catch((err) => {
+			$(".take-photo__error").show();
+			snap.textContent = "Як надати доступ до камери";
+		});
+	snap.addEventListener("click", function () {
+		snapPhoto(webcam, "#canvas");
+		snap.textContent = "Продовжити";
+	});
 };
 const repeatPhoto = () => {
-  $(canvas).removeClass('show')
-  webcamStartFirst()
-  $('.take-photo__instruction').show()
-  $('.take-photo__repeat').removeClass('show')
-  document.querySelector('#photo-do').textContent = " "
-}
-$('.take-photo__repeat').on('click', e => {
-  repeatPhoto()
-})
-webcamStartFirst()
+	$(canvas).removeClass("show");
+	webcamStartFirst();
+	$(".take-photo__instruction").show();
+	$(".take-photo__repeat").removeClass("show");
+	document.querySelector("#photo-do").textContent = "Продолжить";
+};
+$(".take-photo__repeat").on("click", (e) => {
+	repeatPhoto();
+});
+webcamStartFirst();
